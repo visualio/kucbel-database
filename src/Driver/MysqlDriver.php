eb0e714ffd4b35ac83caf35e0e004c7a564a0efb
David Kucbel <david.kucbel@email.cz>
2019-05-04 14:46:46 +0200

    Driver with ms

---
+++ src/Driver/MysqlDriver.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kucbel\Database\Driver;
+
+use DateTimeInterface;
+use Nette;
+
+class MysqlDriver extends Nette\Database\Drivers\MySqlDriver
+{
+	/**
+	 * @param DateTimeInterface $value
+	 * @return string
+	 */
+	function formatDateTime( $value )
+	{
+		return $value->format("'Y-m-d H:i:s.u'");
+	}
+}
